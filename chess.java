1842d10401cfa453cd5e45aa4fd765d3cb5a9a4c
SHAHRIAR-HOSSAIN-RIMON <mdshrhussain@gmail.com>
2025-02-07 22:05:59 +0600

    first commit

---
+++ chess.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+// Represents a position on the chessboard
+class Position {
+    int row, col;
+
+    public Position(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+}
